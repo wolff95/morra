bbf2081c836c6b8b6613c238bd129fcd4ba51434
Warren Wolff <ww.warren.wolff@gmail.com>
2017-09-22 00:18:31 +0200

    First commit

--- app/app.js
+++ app/app.js
@@ -3,12 +3,21 @@
 // Declare app level module which depends on views, and components
 angular.module('myApp', [
   'ngRoute',
-  'myApp.view1',
-  'myApp.view2',
+  'myApp.home',
+  'myApp.scores',
   'myApp.version'
-]).
-config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
-  $locationProvider.hashPrefix('!');
+])
+.config(['$locationProvider', '$routeProvider', function ($locationProvider, $routeProvider) {
 
-  $routeProvider.otherwise({redirectTo: '/view1'});
-}]);
+      $routeProvider.when('/home', {
+        templateUrl: 'home/home.view.html',
+        controller: 'HomeCtrl'
+      })
+
+      $routeProvider.when('/scores', {
+        templateUrl: 'scores/scores.view.html',
+        controller: 'ScoresCtrl'
+      })
+
+      $routeProvider.otherwise({ redirectTo: '/home' });
+    }]);
---
+++ app/scores/scores.controller.js
@@ -0,0 +1,7 @@
+'use strict';
+
+angular.module('myApp.score')
+
+.controller('ScoresCtrl', [function() {
+
+}]);
